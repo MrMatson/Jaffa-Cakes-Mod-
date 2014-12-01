f924e3c4b77556a34dcb8b65c2be3f449cb16f14
MrMatson <matti.rintanen@gmail.com>
2014-12-01 15:00:11 +0200

    New commit

--- src/main/java/com/mrmatson/jaffas/JaffaCakesMod.java
+++ src/main/java/com/mrmatson/jaffas/JaffaCakesMod.java
@@ -1,8 +1,34 @@
 package com.mrmatson.jaffas;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid="Jaffas", name="JaffaCakesMod", version="1.7.10-1.0")
 public class JaffaCakesMod
 {
+
+    @Mod.Instance("Jaffas")
+    public static JaffaCakesMod instance;
+
+
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
 }
